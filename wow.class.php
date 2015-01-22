ac96890f65d60ce01dbfb2e22b7005061ea23ae7
Simon Wallmann <s.wallmann@kompsoft.de>
2015-01-22 23:32:58 +0100

    Bump version

--- wow.class.php
+++ wow.class.php
@@ -27,7 +27,7 @@ if(!class_exists('wow')) {
 	class wow extends game_generic {
 		
 		protected static $apiLevel	= 20;
-		public $version				= '6.1.0';
+		public $version				= '6.1.1';
 		protected $this_game		= 'wow';
 		protected $types			= array('factions', 'races', 'classes', 'talents', 'filters', 'realmlist', 'roles', 'classrole', 'professions', 'chartooltip');	// which information are stored?
 		protected $classes			= array();
