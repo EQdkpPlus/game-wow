7f616ed586243230defba7d222f689b2b39be664
GodMod <GodMod@users.noreply.github.com>
2018-08-13 18:47:16 +0200

    Bump version

--- wow.class.php
+++ wow.class.php
@@ -27,7 +27,7 @@ if(!class_exists('wow')) {
 	class wow extends game_generic {
 
 		protected static $apiLevel	= 20;
-		public $version				= '8.0.22.1'; //Version for EQdkp Plus 2.2
+		public $version				= '8.0.22.2'; //Version for EQdkp Plus 2.2
 		protected $this_game		= 'wow';
 		protected $types			= array('factions', 'races', 'classes', 'talents', 'filters', 'realmlist', 'roles', 'classrole', 'professions', 'chartooltip');	// which information are stored?
 		protected $classes			= array();
