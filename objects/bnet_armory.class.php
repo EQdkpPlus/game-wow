b9c891c0d2d88ffd54399e1a6c5cec058fd32a97
Simon W <s.wallmann@kompsoft.de>
2019-02-08 17:39:43 +0100

    bump versions

--- objects/bnet_armory.class.php
+++ objects/bnet_armory.class.php
@@ -25,7 +25,7 @@ if ( !defined('EQDKP_INC') ){
 
 class bnet_armory extends gen_class {
 
-	private $version		= '8.1.0';
+	private $version		= '8.1.5';
 	private $chariconUpdates = 0;
 	private $chardataUpdates = 0;
 	private $ratepersecond	= 100;
