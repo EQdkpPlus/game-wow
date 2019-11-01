9662957f26ad9b0f11fe7a16e7408f6ade13ae59
GodMod <GodMod@users.noreply.github.com>
2019-11-01 18:25:30 +0100

    Use bigger Icons as default

--- wow.class.php
+++ wow.class.php
@@ -380,10 +380,10 @@ if(!class_exists('wow')) {
 		}
 
 		public function decorate_classes($class_id, $profile=array(), $size=16, $pathonly=false) {
-			$big = ($size > 40) ? '_b' : '';
+			$big = '_b';
 			if(is_file($this->root_path.'games/'.$this->this_game.'/icons/classes/'.$class_id.$big.'.png')){
 				$icon_path = $this->server_path.'games/'.$this->this_game.'/icons/classes/'.$class_id.$big.'.png';
-				return ($pathonly) ? $icon_path : '<img src="'.$icon_path.'" height="'.$size.'" alt="class '.$class_id.'" class="'.$this->this_game.'_classicon classicon'.'" title="'.$this->game->get_name('classes', $class_id).'" />';
+				return ($pathonly) ? $icon_path : '<img src="'.$icon_path.'" height="'.$size.'" alt="class '.$class_id.'" class="'.$this->this_game.'_classicon classicon gameicon'.'" title="'.$this->game->get_name('classes', $class_id).'" />';
 			}
 			return false;
 		}
